8c8663c419cfdc62d5c6c2d38e514c328bded8cc
Nelson Chen <crazysim@gmail.com>
2023-12-26 08:53:49 -0800

    Update USB product and manufacturer strings

--- include/config.h
+++ include/config.h
@@ -22,14 +22,14 @@
 
 // USB device descriptor
 #define USB_VENDOR_ID       0x1189      // VID
-#define USB_PRODUCT_ID      0x8890      // PID
+#define USB_PRODUCT_ID      0x8891      // PID
 #define USB_DEVICE_VERSION  0x0100      // v1.0 (BCD-format)
 
 // USB configuration descriptor
 #define USB_MAX_POWER_mA    50          // max power in mA
 
 // USB descriptor strings
-#define MANUFACTURER_STR    'w','a','g','i','m','i','n','a','t','o','r'
-#define PRODUCT_STR         'M','a','c','r','o','P','a','d'
+#define MANUFACTURER_STR    'm','i','n','d','f','l','a','k','e','s'
+#define PRODUCT_STR         't','o','u','c','h','k','e','y'
 #define SERIAL_STR          'C','H','5','5','2','x','H','I','D'
 #define INTERFACE_STR       'H','I','D','-','K','e','y','b','o','a','r','d'
